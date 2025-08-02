a40da92e53e04c17700c3f1b8cedacdd0d572377
Makvana Sagar <134779010+MakvanaSagar@users.noreply.github.com>
2025-08-02 21:54:06 +0530

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,95 @@
+/*
+
+//Example - 1
+
+function myFunction()	
+{
+	console.log("Hey, I'm sagar!");	//function creation
+}
+myFunction();	//function call
+
+
+
+
+//Example - 2
+ 
+function myNewFun()
+{
+	for(let i=0; i<=5; i++)
+	{
+		console.log(i);
+	}
+}
+myNewFun();
+
+
+
+
+//Example - 3
+
+function printNum(num)
+{
+	console.log("Number is here : ", num);
+}
+printNum(99)
+
+
+
+
+//Example - 4
+
+function numAvg(num1, num2)
+{
+	num = ((num1 + num2)/2)
+	console.log("Number of Average : ", num);
+}
+numAvg(10,100);
+
+
+
+//Example - 5
+
+function mySum(p, q, r)
+{
+	sum = p + q + r;
+	return sum;
+}
+let store = mySum(1,2,3);
+console.log(store);
+
+
+
+
+
+//Example - 6
+
+function joinStr(firstname, lastname)
+{
+	fullname = firstname + " " + lastname;
+	return fullname
+}
+let pic = joinStr("Sagar", "DM");
+console.log(pic);
+
+
+
+//Example - 7 (Function creation using, create a variable)
+
+let Multiply = function(i, j)
+{
+	return i**j;
+}
+console.log(Multiply(10,8));
+
+
+
+//Example - 8	(Function creation using, ARROW Method)
+
+let newFun = (a, b) =>
+{
+	crk = a + b;
+	return crk;
+}
+console.log(newFun(10,99));
+
+*/
